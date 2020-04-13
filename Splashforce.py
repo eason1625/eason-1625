93fa8264976ae153a9b8e93f25a6266fbbd69752
luojilin123 <63606613+luojilin123@users.noreply.github.com>
2020-04-14 01:42:32 +0800

    Add files via upload

---
+++ Splashforce.py
@@ -0,0 +1,17 @@
+from discord import Webhook, RequestsWebhookAdapter, Embed
+
+def avatar_url():
+    return None
+
+def send_webhook():
+    url = 'https://discordapp.com/api/webhooks/699307331191570514/4KaKkDhC2YMVrUt1MwfibEDgBlh-tWe9sd0C1bb1EE0yI_D_K_AKo2NFb8_G5BmnQ6IO'
+    webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
+    embed = Embed(color=272183,title='FY5158 Checkout Successful | Splashforce',url='https://twitter.com/GaneshBot',)\
+    .set_thumbnail(url='https://assets.yeezysupply.com/images/w_937,f_auto,q_auto:sensitive,fl_lossy/c3c8886a281e42e786a9ab95010b82fb_ce49/YEEZY_BOOST_350_V2_LINEN_FY5158_FY5158_04_standard.png')\
+    .add_field(name='PID',value='FY5158',inline=True)\
+    .add_field(name='Size',value='5',inline=True)\
+    .add_field(name='Order Number',value='AYS12345678',inline=True)\
+    .add_field(name='Profile',value='eason',inline=False)\
+    .set_footer(icon_url='https://media.discordapp.net/attachments/284082692591583252/585559939746103308/emote.png',text='Splashforce•2020/04/18')
+    webhook.send(embed=embed,avatar_url=avatar_url(),username="Splashforce")
+send_webhook()
